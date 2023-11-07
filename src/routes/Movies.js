7737c33bb4483ed4369f74c4b1364ef6beecd7a8
kor-seonwoo <tjsdn_9624@naver.com>
2023-11-07 14:14:01 +0900

    Code modification using useQuery of Apollo client

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,26 +1,26 @@
-import { gql, useApolloClient } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 
+const ALL_MOVIES = gql`
+    query getMovies {
+        allMovies {
+            id
+            title
+        }
+    }
+`;
 export default function Movies() {
-    const [movies, setMovies] = useState([]);
-    const client = useApolloClient();
-    useEffect(() => {
-        client.query({
-            query: gql`
-                {
-                    allMovies{
-                        id
-                        title
-                    }
-                }
-            `
-        }).then(results => setMovies(results.data.allMovies));
-    }, [client]);
+    const { data, loading, error } = useQuery(ALL_MOVIES);
+    if (loading) {
+        return <h1>데이터를 받아오는 중!!...</h1>
+    }
+    if (error) {
+        return <h1>서버와 통신이 불안정합니다 잠시후 다시 시도 부탁!!...</h1>
+    }
     return (
         <ul>
-            {movies.map((movie) => (
-                <li key={movie.id}>{movie.title}</li>
-            ))}
+            {
+                data.allMovies.map((movie) => <li key={movie.id}>{movie.title}</li>)
+            }
         </ul>
     );
 }
